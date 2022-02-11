1a7e8dadca74282beaf86bd15e2765e285599653
zebra-lucky <zebra.lucky@gmail.com>
2022-02-12 00:09:46 +0200

    sign-releases.py: add ubuntu 21.10, remove 21.04

--- contrib/sign-releases.py
+++ contrib/sign-releases.py
@@ -137,7 +137,7 @@ PPA_SERIES = {
     'xenial': '16.04.1',
     'bionic': '18.04.1',
     'focal': '20.04.1',
-    'hirsute': '21.04.1',
+    'impish': '21.10.1',
 }
 PEP440_PUBVER_PATTERN = re.compile('^((\d+)!)?'
                                    '((\d+)(\.\d+)*)'
